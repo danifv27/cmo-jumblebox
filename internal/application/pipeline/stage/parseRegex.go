8b27b4f0abf5600a93787f85d17e275c97acd96b
Daniel Fraile Vergel <danifv27@gmail.com>
2023-09-06 17:51:10 +0200

    Add: parseRegex stage to the pipeline

---
+++ internal/application/pipeline/stage/parseRegex.go
@@ -0,0 +1,70 @@
+package stage
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	isplunk "fry.org/qft/jumble/internal/infrastructure/pipeline/splunk"
+)
+
+// RegexParse is a log record parser. Use specific constructors to initialize it.
+type RegexParse struct {
+	format string
+	regexp *regexp.Regexp
+}
+
+// Log line by line parsing.
+// exp  will be transformed to regular expression and used for parsing
+// For example:
+//
+//	`$remote_addr [$time_local] "$request"`
+//
+// It should contain variables in form `$name`. The regular expression will be created using this string format representation
+//
+//	`^(?P<remote_addr>[^ ]+) \[(?P<time_local>[^]]+)\] "(?P<request>[^"]+)"$`
+func NewRegexParse(exp string) *RegexParse {
+	// First split up multiple concatenated fields with placeholder
+	placeholder := " _PLACEHOLDER___ "
+	preparedFormat := exp
+	concatenatedRe := regexp.MustCompile(`[A-Za-z0-9_]\$[A-Za-z0-9_]`)
+	for concatenatedRe.MatchString(preparedFormat) {
+		preparedFormat = regexp.MustCompile(`([A-Za-z0-9_])\$([A-Za-z0-9_]+)(\\?([^$\\A-Za-z0-9_]))`).ReplaceAllString(
+			preparedFormat, fmt.Sprintf("${1}${3}%s$$${2}${3}", placeholder),
+		)
+	}
+	// Second replace each fileds to regexp grouping
+	quotedFormat := regexp.QuoteMeta(preparedFormat + " ")
+	re := regexp.MustCompile(`\\\$([A-Za-z0-9_]+)(?:\\\$[A-Za-z0-9_])*(\\?([^$\\A-Za-z0-9_]))`).ReplaceAllString(
+		quotedFormat, "(?P<$1>[^$3]*)$2")
+	// Finally remove placeholder
+	re = regexp.MustCompile(fmt.Sprintf(".%s", placeholder)).ReplaceAllString(re, "")
+	return &RegexParse{
+		format: exp,
+		regexp: regexp.MustCompile(fmt.Sprintf("^%v", strings.Trim(re, " "))),
+	}
+}
+
+func (p *RegexParse) Parse(input isplunk.SplunkPipeMsg) []isplunk.SplunkPipeMsg {
+
+	var outMsgs []isplunk.SplunkPipeMsg
+
+	if val, ok := input.Get("entry").(string); ok {
+		re := p.regexp
+		fields := re.FindStringSubmatch(val)
+		// fmt.Printf("[DBG]regexp fields: %v", fields)
+		outMsg := isplunk.NewSplunkMessage("parsed.fields", nil)
+		m := make(map[string]string)
+		for i, name := range re.SubexpNames() {
+			if i == 0 {
+				continue
+			}
+			m[name] = fields[i]
+		}
+		outMsg.Add("fields", m)
+
+		return append(outMsgs, outMsg)
+	}
+
+	return outMsgs
+}
--- internal/usecase/logiora/parse_cmd.go
+++ internal/usecase/logiora/parse_cmd.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
-	aconfigurable "fry.org/qft/jumble/internal/application/configurable"
 	apipe "fry.org/qft/jumble/internal/application/pipeline"
-	iTail "fry.org/qft/jumble/internal/infrastructure/follower/tail"
+	"fry.org/qft/jumble/internal/application/pipeline/stage"
+	iTail "fry.org/qft/jumble/internal/infrastructure/follower/un000/tailor"
 	ipipe "fry.org/qft/jumble/internal/infrastructure/pipeline"
 	isplunk "fry.org/qft/jumble/internal/infrastructure/pipeline/splunk"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/speijnik/go-errortree"
 )
 
@@ -24,71 +24,75 @@ func (cmd *ParseCmd) Run(cli *CLI) error {
 	var rcerror, err error
 	var ppln apipe.Piper[isplunk.SplunkPipeMsg]
 	var f *os.File
+	var outCh <-chan isplunk.SplunkPipeMsg
+
 	// var fStat *iTail.FileStat
 	// var positionFile iTail.PositionFile
 
 	if ppln, err = ipipe.Parse[isplunk.SplunkPipeMsg]("pipeline:splunk"); err != nil {
 		return errortree.Add(rcerror, "parsecmd.Run", err)
 	}
+	//TODO: parametrise parsing regular expression
+	regexParser := stage.NewRegexParse("$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" rt=$request_time uct=\"$upstream_connect_time\" uht=\"$upstream_header_time\" urt=\"$upstream_response_time\" x_request-id=\"$http_x_request_id\" reqid=\"$reqid\"")
+
+	st := isplunk.NewSplunkFlatMapStage[isplunk.SplunkPipeMsg](regexParser.Parse)
+	inCh := make(chan isplunk.SplunkPipeMsg)
+	ppln.Next(st)
 	//The source of the pipeline are the lines from the log file
 	if f, err = os.OpenFile(cli.Parse.File, os.O_RDONLY|os.O_SYNC, 0600); err != nil {
 		return errortree.Add(rcerror, "parsecmd.Run", err)
 	}
-	// if fStat, err = iTail.Stat(f); err != nil {
-	// 	return errortree.Add(rcerror, "parsecmd.Run", err)
-	// }
-	//TODO: compute position file from temp dir
-	// if positionFile, err = iTail.OpenPositionFile("./tailPosition.file"); err != nil {
-	// 	return errortree.Add(rcerror, "parsecmd.Run", err)
-	// }
-	configs := []aconfigurable.ConfigurablerFn{
-		// iTail.WithPositionFile(positionFile),
-		iTail.WithDetectRotateDelay(time.Second),
-		iTail.WithWatchRotateInterval(10 * time.Millisecond),
-		// iTail.WithFollowRotate(true),
-	}
-	follow, e := iTail.NewTailFollower(f.Name(), configs...)
-	if e != nil {
+	follow := iTail.NewFollower(f.Name())
+
+	// fmt.Printf("[DBG]Hello World\n")
+	// fmt.Printf("[DBG]cli: %+v\n", *cli)
+	// fmt.Printf("[DBG]ppln: %+v\n", ppln)
+
+	// Create a new context
+	ctx, cancel := context.WithCancel(context.Background())
+	if outCh, err = ppln.Do(ctx, inCh); err != nil {
+		cancel()
 		return errortree.Add(rcerror, "parsecmd.Run", err)
 	}
 
-	fmt.Printf("[DBG]Hello World\n")
-	fmt.Printf("[DBG]cli: %+v\n", *cli)
-	fmt.Printf("[DBG]ppln: %+v\n", ppln)
+	// Pipeline source
+	logEntriesCh, logErrorCh, err := follow.Lines(ctx)
+	if err != nil {
+		cancel()
+		return errortree.Add(rcerror, "parsecmd.Run", err)
+	}
+	go func(ct context.Context) {
+		for {
+			select {
+			case logEntry := <-logEntriesCh:
+				expected := isplunk.NewSplunkMessage("tail.entry", nil)
+				expected.Add("entry", logEntry)
+				inCh <- expected
+			case logError := <-logErrorCh:
+				fmt.Printf("[DBG]error: %v\n", logError)
+				cancel()
+			case <-ct.Done():
+				fmt.Printf("[DBG]context cancelled\n")
+				return
+			}
+		}
+	}(ctx)
 
-	// Create a new context
-	ctx := context.Background()
-
-	logEntriesCh, logErrorCh := follow.Lines(ctx)
-
-waitForCancellation:
+mainLoop:
+	// Drain pipeline
 	for {
 		select {
-		case logEntry := <-logEntriesCh:
-			fmt.Printf("[DBG]entry: %s\n", logEntry)
-		case logError := <-logErrorCh:
-			fmt.Printf("[DBG]error: %v\n", logError)
-			return errortree.Add(rcerror, "parseCmd.Run", err)
+		case msg := <-outCh:
+			spew.Dump("[DBG]msg:", msg)
+			// fmt.Printf("[DBG]msg: %+v\n", msg)
 		case <-ctx.Done():
 			fmt.Printf("[DBG]context cancelled\n")
-			break waitForCancellation
+			break mainLoop
 		}
 	}
+
 	fmt.Printf("[DBG]Goodbye World\n")
+	cancel()
 
 	return nil
 }
-
-// CreateFile creates a file in the temp dir
-// func (d *TempDir) CreateFile(name string) (*os.File, *FileStat) {
-// 	f, err := os.OpenFile(filepath.Join(d.Path, name), os.O_CREATE|os.O_WRONLY|os.O_SYNC, 0600)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	s, err := Stat(f)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	return f, s
-// }
