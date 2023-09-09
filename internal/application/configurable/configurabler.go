2eda1f7760a3eefcc97f878d563b2bd93fe05b2e
Daniel Fraile Vergel <danifv27@gmail.com>
2023-09-09 22:42:22 +0200

    Modifiy:  refactorized Configurabler interface

--- internal/application/configurable/configurabler.go
+++ internal/application/configurable/configurabler.go
@@ -2,6 +2,7 @@ package configurable
 
 type Configurabler interface {
 	Configure(opts ...ConfigurablerFn) error
+	GetInt(key string) (int, error)
 }
 
 // ConfigurablerFn is function that adheres to the Configurabler interface.
